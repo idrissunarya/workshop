8621cfa50f1077adad49af3983e73099c435a7b0
idrissunarya <antimainstream53@gmail.com>
2020-06-10 15:31:59 +0700

    update template

--- apps/dhona/views.py
+++ apps/dhona/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return render (request, 'web/index.jinja')
+    return render (request, 'web/index.html')
     #return HttpResponse('hello django')
 
 def dashboard(request):
